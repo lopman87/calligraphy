a013f040cc032395a8d0caf3aac53efb329be3fd
longpeng05 <longpeng05@meituan.com>
2017-09-11 15:49:36 +0800

    fsa

---
+++ shufa/src/test/java/com/shufa/shufa/ShufaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.shufa.shufa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShufaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
