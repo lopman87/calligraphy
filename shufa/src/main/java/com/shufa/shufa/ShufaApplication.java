04d442db30d26b6809da0f8cc3051d208f481b5c
longpeng05 <longpeng05@meituan.com>
2017-09-11 15:49:23 +0800

    f

---
+++ shufa/src/main/java/com/shufa/shufa/ShufaApplication.java
@@ -0,0 +1,14 @@
+package com.shufa.shufa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShufaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShufaApplication.class, args);
+		
+		
+	}
+}
